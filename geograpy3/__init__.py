4e27d358374f2bcba651eca7cec0b3b07eafa635
tomcsojn <44970244+tomcsojn@users.noreply.github.com>
2020-01-30 14:12:34 +0100

    Update __init__.py

--- geograpy3/__init__.py
+++ geograpy3/__init__.py
@@ -4,12 +4,6 @@ from geograpy3.extraction import Extractor
 from geograpy3.places import PlaceContext
 
 # download all the basic nltk toolkit
-nltk.downloader.download('maxent_ne_chunker')
-nltk.downloader.download('words')
-nltk.downloader.download('treebank')
-nltk.downloader.download('maxent_treebank_pos_tagger')
-nltk.downloader.download('punkt')
-nltk.downloader.download('averaged_perceptron_tagger')
 
 
 def get_place_context(url = None, text = None):
